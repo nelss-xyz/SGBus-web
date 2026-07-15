98e15f6105480801f2e3f72deafc9ece62463f24
Nelson <nelso.n@aol.com>
2026-07-15 10:17:57 +0800

    [TEMPORARY] Bypass cache for MRT Data

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
       },
       {
         source: '/api/v2/data/mrt',
-        destination: 'https://cdn.jsdelivr.net/gh/nelss-xyz/SGTransitData/Data/Output/mrt/mrt.json',
+        destination: 'https://raw.githubusercontent.com/nelss-xyz/SGTransitData/refs/heads/main/Data/Output/mrt/mrt.json',
         statusCode: 301,
       },
     ]
